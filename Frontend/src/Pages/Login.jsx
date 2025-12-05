43d6e62a7fc94a9f8b6b777c30fac88743318d6a
Suyash1505 <adv.mks7171@gmail.com>
2025-12-06 02:22:35 +0530

    created login page

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -14,16 +14,20 @@ const Login = () => {
 
   return (
     <form className='min-h-[80vh] flex items-center'>
-      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl border-gray-600 text-sm shadow-xl'>
         <p className='text-2xl font-semibold'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
-        <p>Please {state === 'Sign Up' ? "sign up" : "login"} to book appointment</p>
+        
+        <p className='text-gray-400'>
+          Please {state === 'Sign Up' ? "sign up" : "login"} to book appointment
+        </p>
 
         {/* -------------- INPUT FIELDS - FULL NAME ---------------- */}
         {
           state === 'Sign Up' && <div className='w-full'>
             <p>Full Name</p>
+
             <input 
-              className='border border-zinc-300 rounded w-full p-2 mt-1'
+              className='border border-gray-600 rounded w-full p-2 mt-1'
               type="text"
               onChange={(e) => setName(e.target.name)}
               value={name}
@@ -35,8 +39,9 @@ const Login = () => {
         {/* -------------- INPUT FIELDS - EMAIL ---------------- */}
         <div className='w-full'>
           <p>Email</p>
+
           <input 
-            className='border border-zinc-300 rounded w-full p-2 mt-1'
+            className='border border-gray-600 rounded w-full p-2 mt-1'
             type="email"
             onChange={(e) => setEmail(e.target.name)}
             value={email}
@@ -48,7 +53,7 @@ const Login = () => {
         <div className='w-full'>
           <p>Password</p>
           <input 
-            className='border border-zinc-300 rounded w-full p-2 mt-1'
+            className='border border-gray-600 rounded w-full p-2 mt-1'
             type="password"
             onChange={(e) => setPassword(e.target.name)}
             value={password}
@@ -57,14 +62,15 @@ const Login = () => {
         </div>
 
         <button
-          className='bg-primary text-white w-full py-2 text-base rounded-md'
+          className='bg-primary text-gray-400 w-full py-2 text-base rounded-md'
         >
           {state === 'Sign Up' ? "Create Account": "Login"}
         </button>
 
         {
           state === 'Sign Up' 
-          ? <p>Already have an account? 
+          ? <p className='text-gray-400'>
+              Already have an account? 
               <span 
               onClick={() => setState('Login')}
               className='text-primary cursor-pointer underline'
@@ -74,7 +80,7 @@ const Login = () => {
             
             </p>
 
-          : <p>Create a new account? 
+          : <p className='text-gray-400'>Create a new account? 
               <span 
                 onClick={() => setState('Sign Up')} 
                 className='text-primary cursor-pointer underline'
